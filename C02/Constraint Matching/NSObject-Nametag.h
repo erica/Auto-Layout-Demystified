21cfb0ea73d529c6c737e3327ebec3568c701a99
Erica Sadun <ericasadun@Esopus-Spitzenburg.local>
2013-03-06 13:25:29 -0700

    C02

---
+++ C02/Constraint Matching/NSObject-Nametag.h
@@ -0,0 +1,17 @@
+/*
+ 
+ Erica Sadun, http://ericasadun.com
+ 
+ */
+
+#import <Foundation/Foundation.h>
+
+// A nametag is an associated string object that can be assigned to any object.
+// Similar in intent and nature to UIView's "tag" property, it allows you to
+// assing readable text to objects for annotation and searching.
+
+#pragma mark - Support nametags universally
+@interface NSObject (Nametags)
+@property (nonatomic, strong) NSString *nametag;
+@end
+
