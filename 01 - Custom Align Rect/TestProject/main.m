d71a7dfceaccba8710e19fc5d024bb6a4d78e097
Erica Sadun <ericasadun@Esopus-Spitzenburg.local>
2013-03-06 13:17:37 -0700

    C01

---
+++ 01 - Custom Align Rect/TestProject/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TestProject
+//
+//  Created by Erica Sadun on 12/24/12.
+//  Copyright (c) 2012 Erica Sadun. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
